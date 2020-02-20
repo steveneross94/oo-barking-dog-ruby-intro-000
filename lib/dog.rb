1e34e6ea2b7f6c32fccd6ac6e79bd7be7cfccfed
Steven Ross <ross.steven@me.com>
2020-02-20 00:09:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,7 @@
 # Your code goes here!
 class Dog
   def initialize(name)
-    def name=(name)
-      @name = name
-    end
-    def name
-      @name
-    end
+
     def bark
       puts "woof!"
     end
